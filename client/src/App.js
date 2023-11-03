b8b0c9a10e037272c7a964b387de23cc7f2b22a1
prabhat-4165 <prabhu27082001@gmail.com>
2023-11-04 01:19:24 +0530

    updated app.js to import and export login, signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,44 @@
 import React from 'react'
 import './App.css';
-import  LoginSignup  from './Components/LoginSignup/LoginSignup.jsx';
+// import  LoginSignup  from './Components/LoginSignup/LoginSignup.jsx';
+import  Signup  from './Components/LoginSignup/Signup.js';
+import  Login  from './Components/LoginSignup/Login.js';
+import { Link } from "react-router-dom";
+// import Profile from './Profile.js';
+import Navbar from './Components/Navbar/Navbar.js'
+import MyAccount from './Components/MyAccount/MyAccount.js'
+import {  Route, Routes,  BrowserRouter } from 'react-router-dom';
+
+function HomePage(){
+  return (
+    <>
+      <h1>
+         <p><Link to="/Signup" className="Signup">Signup here</Link></p>
+         <p><Link to="/MyAccount" className="MyAccount">My account</Link></p> 
+         <p><Link to="/navbar" className="navbar">check out navbar</Link></p> 
+      </h1>
+    </>
+  )
+}
+
 function App() {
   return (
-    <div >
-     <LoginSignup/>
-    </div>
+    <>
+     
+     <div className="link-container">
+     <BrowserRouter>
+  <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route exact path="/Signup" element={<Signup />} />
+        <Route exact path="/MyAccount" element={<MyAccount />} />
+        <Route exact path="/navbar" element={<Navbar />} />
+        <Route exact path="/login" element={<Login />} />
+        
+  </Routes>
+</BrowserRouter>
+      </div>
+    </>
+    
   );
 }
 
