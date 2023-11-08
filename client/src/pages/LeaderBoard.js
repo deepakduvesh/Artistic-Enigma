fc8475179b10e884929b17fb706fc7ecf251777a
prabhat-4165 <prabhu27082001@gmail.com>
2023-11-08 16:44:49 +0530

    Created LeaderBoard Component and added it's css

---
+++ client/src/pages/LeaderBoard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './LeaderBoard.css';
+ const LeaderBoard = () => {
+  return (
+    <>
+     {/* leaderboard  */}
+     <div className="leaderboard">
+     <h1>Here is a leaderboard</h1>
+     </div>
+     </>
+  )
+}
+
+export default LeaderBoard;
