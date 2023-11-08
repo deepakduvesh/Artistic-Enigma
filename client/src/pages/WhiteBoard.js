81d0aad86c6f3641b4e39c97c8d93c25d6f15127
prabhat-4165 <prabhu27082001@gmail.com>
2023-11-08 16:44:15 +0530

    Created WhiteBoard Component and added it's css

---
+++ client/src/pages/WhiteBoard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './WhiteBoard.css';
+
+ const WhiteBoard = () => {
+  return (
+    <>
+    {/* white board  */}
+    <div className="white-board">
+    <h1> whiteboard.</h1>
+    </div>
+    </>
+  )
+}
+
+export default WhiteBoard;
