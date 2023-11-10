c14d612c256ca776cc4c813a5d6ed92d8fbcb2cc
deepakduvesh <deepakduvesh356@gmail.com>
2023-11-10 12:41:43 +0530

    user will wait here until lobby is full

--- client/src/Components/Lobby.js
+++ client/src/Components/Lobby.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../Styles/Lobby.css';
-
-const Lobby = () => {
+const Lobby =  ({count}) => {
+ 
   return (
     <div className="Lobby-container">
         <div className="Lobby-box">
             <h1>Welcome to the Game Lobby</h1>
             <div className="count">
                 <p>Tottal number of player joined are:</p>
-                <span>here write count</span>
+                <span>total player in lobby {count} out of 3</span>
             </div>
 
             <div className="enter-game">
