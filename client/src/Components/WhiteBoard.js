09c961d8a63eb74bb31d7c5a2224fa7d47afe11f
prabhat-4165 <prabhu27082001@gmail.com>
2023-11-22 02:45:18 +0530

    updated css of play page

--- client/src/Components/WhiteBoard.js
+++ client/src/Components/WhiteBoard.js
@@ -388,7 +388,7 @@ import Time from './Time.js';
 					</button>
 				   
 
-				{/* <p>Tools</p> */}
+				<p>Tools</p>
 					
 			</div>
 					
@@ -402,7 +402,7 @@ import Time from './Time.js';
 					<button onClick={() => setMode('circle')}>
 					<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg>
 					</button>
-					{/* <p>Shapes</p> */}
+					<p>Shapes</p>
 			</div>
 			
 			<div className="undo-redo-buttons">
