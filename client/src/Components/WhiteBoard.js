7267b0751d20314b5044a3ca1ae4ff324c5411d9
prabhat-4165 <prabhu27082001@gmail.com>
2023-11-23 15:58:54 +0530

    added guessed feature and svg icons in tools

--- client/src/Components/WhiteBoard.js
+++ client/src/Components/WhiteBoard.js
@@ -439,8 +439,12 @@ import sound from '../Assets/play.wav';
 			</div>
 			
 			<div className="undo-redo-buttons">
-			<button className='tools-button' onClick={undo}>Undo</button>
-			<button className='tools-button' onClick={redo}>Redo</button>
+			<button className='tools-button' onClick={undo}>
+			<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M125.7 160H176c17.7 0 32 14.3 32 32s-14.3 32-32 32H48c-17.7 0-32-14.3-32-32V64c0-17.7 14.3-32 32-32s32 14.3 32 32v51.2L97.6 97.6c87.5-87.5 229.3-87.5 316.8 0s87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3s-163.8-62.5-226.3 0L125.7 160z"/></svg>
+			</button>
+			<button className='tools-button' onClick={redo}>
+			<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M386.3 160H336c-17.7 0-32 14.3-32 32s14.3 32 32 32H464c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v51.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0s-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3s163.8-62.5 226.3 0L386.3 160z"/></svg>
+			</button>
 			</div>
 
 			</div>
@@ -481,7 +485,9 @@ import sound from '../Assets/play.wav';
     </div>
 
 	<div className="download-button">
-		<button onClick={takeScreenShot} >Download</button>	
+		<button onClick={takeScreenShot} >
+		<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>	
+		</button>	
 		 </div>
 	
     </>
