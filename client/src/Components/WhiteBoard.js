c7ab795dd8c1b19e4a485fdf23d12d840dbf3f72
Shivam Kumar <shivamkrmnnit@gmail.com>
2023-11-23 16:33:34 +0530

    added configurer show name and hide name buttons

--- client/src/Components/WhiteBoard.js
+++ client/src/Components/WhiteBoard.js
@@ -135,10 +135,14 @@ import sound from '../Assets/play.wav';
 				else if(mode === 'bucket'){
 					setCurrentCursor('buckets')
 				}
+				else if(mode === 'name'){
+					setCurrentCursor('names')
+				}
 				
 				drawHistory();
 				
 		}
+		
 
 		const drawHistory = () =>{
 			const newCurrentState = canvas.toDataURL();
@@ -350,6 +354,11 @@ import sound from '../Assets/play.wav';
 				return (
 					<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>
 				);
+				case 'names':
+					return(
+						<p>name</p>
+						
+					)
 		  default:
 			return null;
 		}
@@ -419,7 +428,7 @@ import sound from '../Assets/play.wav';
 					<button onClick={() => setMode('clear')}>
 					<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>
 					</button>
-				   
+					
 
 				<p>Tools</p>
 					
@@ -445,6 +454,8 @@ import sound from '../Assets/play.wav';
 			<button className='tools-button' onClick={redo}>
 			<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M386.3 160H336c-17.7 0-32 14.3-32 32s14.3 32 32 32H464c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v51.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0s-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3s163.8-62.5 226.3 0L386.3 160z"/></svg>
 			</button>
+
+			
 			</div>
 
 			</div>
@@ -484,11 +495,23 @@ import sound from '../Assets/play.wav';
 		</div>
     </div>
 
-	<div className="download-button">
+	    <div className="download-button">
 		<button onClick={takeScreenShot} >
 		<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>	
 		</button>	
 		 </div>
+
+		    <div className='configure-names'>
+			<button onClick={() => setMode('name')}>
+						<p>show name</p>
+						
+					</button>
+
+					<button onClick={() => setMode('rect')}>
+						<p>hide name</p>
+						
+					</button>
+			</div>
 	
     </>
   )
