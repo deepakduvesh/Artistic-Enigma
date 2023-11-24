8c492b2c80cf715231c1417a004bdb8cc5ccc250
deepakduvesh <deepakduvesh356@gmail.com>
2023-11-24 23:38:41 +0530

    leaderboard

--- client/src/Components/LeaderBoard.js
+++ client/src/Components/LeaderBoard.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import '../Styles/LeaderBoard.css';
 import {socket} from "../App.js"
  const LeaderBoard = ({username, id,email}) => {
-  const player = null
   // const [map,setmap] = useState(Map)
   // useEffect(() => {
     // let mp =  new Map()
